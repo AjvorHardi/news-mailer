45f138e41297bcc8ef7828546d4e03204ae8a507
AjvorHardi <markovic.goran@outlook.com>
2026-06-23 13:14:20 +0200

    chore: prepare vercel deployment

--- src/app/layouts/DemoLayout.tsx
+++ src/app/layouts/DemoLayout.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router';
 import { WorkspaceLayout } from './WorkspaceLayout';
-import { DEMO_NEWSLETTER_ID } from '../../lib/demo-storage';
 import { Button } from '../../shared/ui/Button';
 
 export function DemoLayout() {
@@ -9,7 +8,7 @@ export function DemoLayout() {
   return (
     <WorkspaceLayout
       basePath="/demo"
-      footerLabel={`Demo data: ${DEMO_NEWSLETTER_ID}`}
+      footerLabel="DEMO WORKSPACE"
       footerAction={
         <Button
           type="button"
